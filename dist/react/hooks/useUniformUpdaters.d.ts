3943956b4d765f81cb80a97cf8584ad8ed051111
Michael Cullan <mjcullan@gmail.com>
2025-07-18 05:25:54 -0700

    amend

--- dist/react/hooks/useUniformUpdaters.d.ts
+++ dist/react/hooks/useUniformUpdaters.d.ts
@@ -1,4 +1,4 @@
-import { UniformParam, UniformType, UniformUpdaterDef } from '../../types';
+import { UniformParam, UniformUpdaterDef } from '../../types';
 export declare const useUniformUpdaters: (programId: string, uniforms: Record<string, UniformParam>) => {
-    [x: string]: UniformUpdaterDef<UniformType>[];
+    [x: string]: UniformUpdaterDef<import('../../types').UniformType>[];
 };
