9a12259af1411cc477c4b52cc8a9d4cbac3409c1
micu <mjcullan@gmail.com>
2025-07-18 05:19:24 -0700

    Fix type (#23)

--- src/react/hooks/useUniformUpdaters.ts
+++ src/react/hooks/useUniformUpdaters.ts
@@ -1,14 +1,14 @@
 import { useMemo } from 'react';
 
 import { createCommonUpdaters, createUniformUpdater } from '@/react/lib/createUniformUpdater';
-import type {  UniformParam, UniformType, UniformUpdaterDef } from '@/types';
+import type {  UniformParam, UniformUpdaterDef } from '@/types';
 
 export const useUniformUpdaters = (
     programId: string,
     uniforms: Record<string, UniformParam>
 ) => {
     return useMemo(() => {
-        const updaters: UniformUpdaterDef<UniformType>[] = createCommonUpdaters();
+        const updaters: UniformUpdaterDef[] = createCommonUpdaters();
 
         Object.entries(uniforms).forEach(([name, param]) => {
             const uniformName = name.startsWith('u_') ? name : `u_${name}`;
